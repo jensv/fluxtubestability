c312a14be5e0d26f9855d89eafd9eae1c81f47e7
Jens vdL <jensvonderlinden@gmail.com>
2014-06-05 12:54:29 -0700

    debugging, fixing errors as they occur while trying to integrate in notebook

--- eigenvalue_problem.py
+++ eigenvalue_problem.py
@@ -5,18 +5,17 @@ Created on Tue May 27 13:52:26 2014
 @author: Jens von der Linden
 """
 
-#Python 3.x compatability
+
 from __future__ import print_function
-from __future__ import unicode_literals
 from __future__ import division
 from __future__ import absolute_import
-from future.builtins.disabled import (apply, cmp, coerce, execfile,
-                                      file, long, raw_input, reduce, reload,
-                                      unicode, xrange, StandardError)
-from future.types import bytes, dict, int, range, str
-from future.builtins.misc import (ascii, chr, hex, input, next,
-                                  oct, open, round, super)
-from future.builtins.iterators import filter, map, zip
+from future.builtins import (ascii, bytes, chr, dict, filter, hex, input,
+                             int, map, next, oct, open, pow, range, round,
+                             str, super, zip)
+"""Python 3.x compatability above"""
+
+"""Numba jit accelerate
+from numbapro import jit, float64, void"""
 
 import scipy.integrate as inte
 import scipy.special as spec
@@ -37,20 +36,23 @@ def xi_boundary(params, r):
     return factor*num / den*pressure(a)
 
 
-def xi_integrate(params, atol, rtol, r_init, dr, r_max):
+"""@jit(float64[:](void, float64, float64, float64, float64, float64))"""
+def xi_integrate(params, r_init, dr, r_max, atol=None, rtol=None):
     r"""
     """
     xi = []
 
     xi_int = inte.ode(xi_der)
-
-    xi_int.set_integrator('dopri5', atol, rtol)
+    if atol is None or rtol is None:
+        xi_int.set_integrator('dopri5')
+    else:
+        xi_int.set_integrator('dopri5', atol, rtol)
     xi_int.set_initial_value(xi_initial_condition(params, r_init))
     xi_int.set_f_params(params)
-
     while xi_int.successful() and xi_int.t < r_max-dr:
         xi_int.integrate(xi_int.t + dr)
         xi.append(xi_int.y)
+        print((xi_int.successful(), xi_int.t))
     return np.array(xi)
 
 
@@ -72,36 +74,47 @@ def xi_integrate_boundary():
     pass
 
 
+"""@jit(float64[:](float64, float64[:], void))"""
 def xi_der(r, xi_vec, params):
     r"""
     """
     xi_d = np.zeros(2)
-    xi_d[0] = (c1(params, r)/jardin_d(params, r)*xi_vec[0] - r*c2(params, r)
-               * xi_vec[1])
-    xi_d[1] = 1/r*c3(params, r)*xi_vec[0] - c1(params, r)*xi_vec[1]
+    if r == 0:
+        xi_d[0] = 0
+        xi_d[1] = 0
+    else:
+        p_term, f_difference, f_term = repeating_terms(params, r)
+        xi_d[0] = (c1(params, r, p_term, f_difference, f_term) /
+                   jardin_d(params, r, p_term, f_difference, f_term)*xi_vec[0]
+                   - r*c2(params, r, p_term, f_difference, f_term)
+                   * xi_vec[1])
+        xi_d[1] = (1/r*c3(params, r, p_term, f_difference, f_term)*xi_vec[0]
+                   - c1(params, r, p_term, f_difference, f_term)*xi_vec[1])
     return xi_d
 
 
+"""@jit(float64(void, float64))"""
 def jardin_f(params, r):
     r"""
     """
     k, m, b_theta, b_z = dc.retrieve(params, ('k', 'm', 'b_theta', 'b_z'))
     return m/r*b_theta(r)-k*b_z(r)
 
-
+"""@jit(float64(void, float64, float64, float64, float64))"""
 def c1(params, r, p_term, f_difference, f_term):
     r"""
 
     """
     (m, mu0, omega_sq,
      rho, b_theta) = dc.retrieve(params, ('m', 'mu0', 'omega_sq',
-                                          'pressure', 'rho', 'b_theta'))
+                                          'rho', 'b_theta'))
     factor = 2*b_theta(r)/(mu0*r)
     term1 = rho(r)**4*omega_sq**2*b_theta(r)
     term2_factor = -m/r*jardin_f(params, r)
     return factor*(term1 + term2_factor * (p_term - f_term))
 
 
+"""@jit(float64(void, float64, float64, float64, float64))"""
 def c2(params, r, p_term, f_difference, f_term):
     r"""
 
@@ -121,9 +134,10 @@ def c3(params, r, p_term, f_difference, f_term):
                                                    'state_gamma', 'pressure',
                                                    'rho', 'b_theta', 'b_z',
                                                    'd_b_theta_over_r'))
-    term1 = jardin_d(params, r)*(rho(r)*omega_sq - jardin_f(params, r)/mu0 +
-                                 2*b_theta(r)/mu0*d_b_theta_over_r(r))
-    term2 = rho*omega_sq*f_difference*((2*b_theta(r)**2)/(mu0*r))**2
+    term1 = (jardin_d(params, r, p_term, f_difference, f_term)*
+             (rho(r)*omega_sq - jardin_f(params, r)/mu0 +
+                                 2*b_theta(r)/mu0*d_b_theta_over_r(r)))
+    term2 = rho(r)*omega_sq*f_difference*((2*b_theta(r)**2)/(mu0*r))**2
     term3 = (-(state_gamma*pressure(r)*f_difference + rho(r)*omega_sq*b_z(r)**2
                / mu0)*(2*b_theta(r)*jardin_f(params, r)/(mu0*r)))
     return term1 + term2 + term3
@@ -135,7 +149,7 @@ def repeating_terms(params, r):
     (mu0, omega_sq, state_gamma, pressure, rho, b_theta,
      b_z) = dc.retrieve(params, ('mu0', 'omega_sq', 'state_gamma', 'pressure',
                                  'rho', 'b_theta', 'b_z'))
-    b_sq = b_theta**2 + b_z**2
+    b_sq = b_theta(r)**2 + b_z(r)**2
     p_term = rho(r)*omega_sq*(state_gamma*pressure(r) + b_sq / (2*mu0))
     f_difference = rho(r)*omega_sq - jardin_f(params, r)**2/mu0
     f_term = state_gamma*pressure(r)*jardin_f(params, r)**2/mu0
@@ -151,6 +165,6 @@ def jardin_d(params, r, p_term, f_difference, f_term):
                                                'b_z'))
     factor1 = (rho(r)*omega_sq-jardin_f(params, r)**2/mu0)
     factor2_term1 = (rho(r)*omega_sq*(state_gamma*rho(r) +
-                                      (b_theta(r)**2+b_z**2)/(2*mu0)))
-    factor2_term2 = -state_gamma*rho(r)*jardin_f**2/mu0
+                                      (b_theta(r)**2+b_z(r)**2)/(2*mu0)))
+    factor2_term2 = -state_gamma*rho(r)*jardin_f(params, r)**2/mu0
     return factor1*(factor2_term1 + factor2_term2)
