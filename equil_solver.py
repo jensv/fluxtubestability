7870a4544b37b480f53c4e657dad8fc19eb63155
Jens vdL <jensvonderlinden@gmail.com>
2014-06-05 12:55:23 -0700

    debugging fixing errors occuring in conjunction with eigenvalue_problem

--- equil_solver.py
+++ equil_solver.py
@@ -26,8 +26,16 @@ class Equil_solver(object):
     def set_splines(self, param_points):
         splines = {}
         r = self.r
+        d_b_theta_r_request = False
         for key, value in param_points.items():
-            splines[key] = interp.InterpolatedUnivariateSpline(r, value, k=3)
+            if key == 'd_b_theta_over_r':
+                d_b_theta_r_request = True
+            else:
+                splines[key] = interp.InterpolatedUnivariateSpline(r, value,
+                                                                   k=3)
+        if d_b_theta_r_request:
+            splines['d_b_theta_over_r'] = self.d_b_theta_over_r(r,
+                                                           splines['b_theta'])
         self.splines = splines
 
     def get_splines(self):
@@ -36,21 +44,24 @@ class Equil_solver(object):
 
 class Parabolic_nu2(Equil_solver):
 
-    def __init__(self, a=1, points=500, q0=1.0, k=1, b_z0=1):
+    def __init__(self, a=1, points=500, q0=1.0, k=1, b_z0=1, temp=1.0):
         self.r = np.linspace(0, a, points)
         self.q0 = q0
         self.k = k
         self.b_z0 = b_z0
+        self.temp = temp
         r = self.r
         self.j0 = self.get_j0()
         param_points = {'j_z': self.j_z(r), 'b_theta': self.b_theta(r),
                         'b_z': self.b_z(r), 'pprime': self.pprime(r),
-                        'pressure': self.pressure(r)}
+                        'pressure': self.pressure(r), 'q': self.q(r),
+                        'rho': self.rho(r),
+                        'd_b_theta_over_r': None}
         self.set_splines(param_points)
 
     def get_j0(self):
         self.j0 = 1
-        return self.k*self.b_z(0)/self.b_theta_over_r(0)
+        return self.k*2*self.b_z(0)/self.q0
 
     def j_z(self, r):
         return self.j0*(1 - r**2)**2
@@ -58,10 +69,6 @@ class Parabolic_nu2(Equil_solver):
     def b_theta(self, r):
         j0 = self.j0
         return j0*r/2 - j0*r**3/2 + j0*r**5/6
-
-    def b_theta_over_r(self, r):
-        j0 = self.j0
-        return j0*1/2 - j0*r**2/2 + j0*r**4/6
 
     def b_z(self, r):
         b_z0 = self.b_z0
@@ -79,6 +86,19 @@ class Parabolic_nu2(Equil_solver):
         j0 = self.j0
         return (47*j0**2/720 - j0**2*r**2/4 + 3*j0**2*r**4/8 - 5*j0**2*r**6/18
                 + 5*j0**2*r**8/48 - j0**2*r**10/60)
+
+    def rho(self, r):
+        return self.pressure(r)/self.temp
+
+    def q(self, r):
+        q_to_return = r*self.k*self.b_z(r)/self.b_theta(r)
+        q_to_return[0] = self.q0
+        return q_to_return
+
+    def d_b_theta_over_r(self, r, b_theta):
+        b_over_r = interp.InterpolatedUnivariateSpline(r, b_theta.derivative()(r)
+                                                       /r, k=3)
+        return b_over_r.derivative()
 
 
 class Smoothed_core_skin(Equil_solver):
