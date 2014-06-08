ed2955b3d5e6d5c0d94069d4281c93e97556907b
Jens vdL <jensvonderlinden@gmail.com>
2014-06-07 19:18:28 -0700

    Added equations to docstrings

--- newcomb.py
+++ newcomb.py
@@ -7,7 +7,6 @@ Created on Mon May 19 10:03:00 2014
 
 
 from __future__ import print_function
-from __future__ import unicode_literals
 from __future__ import division
 from __future__ import absolute_import
 from future.builtins import (ascii, bytes, chr, dict, filter, hex, input,
@@ -20,6 +19,7 @@ import numpy as np
 import scipy.interpolate as interpolate
 import scipy.integrate as integrate
 import dict_convenience as dc
+import sys
 sys.path.append("../Scripts")
 import dict_convenience
 import equil_solver
@@ -62,6 +62,7 @@ def f_eq(r, k, m, b_z, b_theta):
     Newcomb (1960) Hydromagnetic Stability of a Diffuse Linear Pinch
     Equation (16)
     """
+    f_eq()
     return r*f_num_wo_r(r, k, m, b_z, b_theta)/f_denom(r, k, m)
 
 
@@ -132,7 +133,7 @@ def f_num_wo_r(r, k, m, b_z, b_theta):
     Newcomb (1960) Hydromagnetic Stability of a Diffuse Linear Pinch
     Equation (16)
     """
-    return (k*r*b_z + m*b_theta)**2
+    return (k*r*b_z(r) + m*b_theta(r))**2
 
 
 def g_eq_18(r, k, m, b_z, b_theta, p_prime):
@@ -166,6 +167,13 @@ def g_eq_18(r, k, m, b_z, b_theta, p_prime):
 
     Notes
     -----
+    Implements equation
+    .. math::
+        \frac{2 k^{2} r^{2}}{k^{2} r^{2} + m^{2}} \ frac{dP}{dr} +
+        \frac{1}{r}(k r B_{z}+m B_{\theta})^{2}
+        \frac{k^{2}r^{2}+m^{2}-1}{k^{2}r^{2}+m^{2}}+
+        \frac{2k^{2}r}{(k^{2}r^{2}+m^{2})^{2}}
+        (k^{2}r^{2}B_{z}^{2}-m^{2}B_{theta}^{2})
 
     Reference
     ---------
@@ -176,10 +184,14 @@ def g_eq_18(r, k, m, b_z, b_theta, p_prime):
     -----
     Equation (17) is harder to implement due to derivative of Newcomb f.
     """
-    term1 = 2*k**2*r**2/(f_denom(r, k, m)) * p_prime
-    term2 = (1/r*f_num_wo_r(r, k, m, b_z, b_theta)*(k**2*r**2+m**2-1) /
-             f_denom(r, k, m))
-    term3 = 2*k**2*r/f_denom(r, k, m)**2*(k**2*r**2*b_z**2-m**2*b_theta**2)
+    term1 = 2*k**2*r**2/(f_denom(r, k, m)) * p_prime(r)
+    if r == 0.:
+        term2 = k*b_z(r)*(k**2*r**2+m**2-1) / f_denom(r, k, m)
+    else:
+        term2 = (1/r*f_num_wo_r(r, k, m, b_z, b_theta)*(k**2*r**2+m**2-1) /
+                 f_denom(r, k, m))
+    term3 = (2*k**2*r/f_denom(r, k, m)**2*
+             (k**2*r**2*b_z(r)**2-m**2*b_theta(r)**2))
     return term1 + term2 + term3
 
 
@@ -261,7 +273,7 @@ def newcomb_der_divide_f(t, y, k, m, b_z, b_theta, p_prime):
     return y_prime
 
 
-def newcomb_int(divide_f, atol, rtol, rmax, dr, params):
+def newcomb_int(divide_f, r_max, dr, params, atol=None, rtol=None):
     r"""
     Integrate Newcomb's Euler Lagrange equation as two odes.
 
@@ -297,25 +309,28 @@ def newcomb_int(divide_f, atol, rtol, rmax, dr, params):
     Alan Glasser (unknown) Cyl code
     """
     (k, m, b_z, b_theta, p_prime) = dc.retrieve(params, ['k', 'm', 'b_z',
-                                                         'b_theta', 'p_prime'])
+                                                         'b_theta', 'pprime'])
     xi = []
     if divide_f:
         xi_int = integrate.ode(newcomb_der_divide_f)
     else:
         xi_int = integrate.ode(newcomb_der)
 
-    xi_int.set_integrator('lsoda', atol, rtol)
-    xi_int.set_initial_value()
+    if not (atol and rtol):
+        xi_int.set_integrator('lsoda')
+    else:
+        xi_int.set_integrator('lsoda', atol, rtol)
+    xi_int.set_initial_value([0, 0], 0)
     xi_int.set_f_params(k, m, b_z, b_theta, p_prime)
 
     while xi_int.successful() and xi_int.t < r_max-dr:
         xi_int.integrate(xi_int.t + dr)
-        xi.append = xi
-        crossing_condition(xi)
+        xi.append(xi_int.y)
+        #crossing_condition(xi)
     return np.array(xi)
 
 
-def suydam(r, b_z, qprime, q, pprime):
+def suydam(r, b_z, q_prime, q, p_prime):
     r"""
     Returns suydam condition.
 
@@ -350,15 +365,13 @@ def suydam(r, b_z, qprime, q, pprime):
     Reference
     ---------
     Newcomb (1960) Hydromagnetic Stability of a Diffuse Linear Pinch
-
-    Example
-    -------
-
+    eq (5).
+    Jardin (2010) Computational Mehtods in Plasma Physics. eq (8.84)
     """
-    return r/8*b_z*(qprime/q)**2+pprime
+    return r/8*b_z*(q_prime/q)**2+p_prime
 
 
-def check_suydam(r, qprime, q, pprime):
+def check_suydam(r, q_prime, q, p_prime):
     r"""
     Parameters
     ----------
@@ -376,10 +389,10 @@ def check_suydam(r, qprime, q, pprime):
     -------
 
     """
-    if ((suydam(r, qprime, q, pprime) <= 0).sum() == 0):
+    if ((suydam(r, q_prime, q, p_prime) <= 0).sum() == 0):
         return (False, np.array([]))
     else:
-        return (True, r[(suydam(r, qprime, q, pprime) <= 0)])
+        return (True, r[(suydam(r, q_prime, q, pprime) <= 0)])
 
 
 def check_sing(r, k, b_z, m, b_theta, tol):
