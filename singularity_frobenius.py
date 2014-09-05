239af81c14e703dfb4efc1d05229c8416c289e2e
Jens vdL <jensvonderlinden@gmail.com>
2014-09-05 14:05:13 -0700

    Add docstrings, Add mutiply by f to frob solution
    The small solution of the frobenius equation must
    be modified to satisfy the y form of the ODE set.
    y[1] must be multiplied by f. This is still under
    construction.

--- singularity_frobenius.py
+++ singularity_frobenius.py
@@ -22,6 +22,24 @@ import numpy as np
 def alpha_func(r, b_z, b_z_prime, b_theta, b_theta_prime, **kwargs):
     r"""
     Return alpha for Frobenius solution.
+
+    Parameters
+    ----------
+    r : ndarray of floats
+        radial position of singularity
+    b_z : ndarray of floats
+        axial magnetic field
+    b_z_prime : ndarray of floats
+        derivative of axial magnetic field
+    b_theta : ndarry of floats
+        azimuthal magnetic field
+    b_theta_prime : ndarry of floats
+        derivative of azimuthal magnetic field
+
+    Returns
+    -------
+    alpha : ndarray of floats
+        beta of quadratic equation for the exponents of the Forbenius solutions
     """
     mu = b_theta/(r*b_z)
     mu_prime = (r*b_z*b_theta_prime - b_theta*(b_z + r*b_z_prime)) / (r*b_z)**2
@@ -31,6 +49,19 @@ def alpha_func(r, b_z, b_z_prime, b_theta, b_theta_prime, **kwargs):
 def beta_func(b_z, b_theta, p_prime, **kwargs):
     r"""
     Return beta for Frobenius solution.
+
+    Parameters
+    ----------
+    b_z : ndarray of floats
+        axial magnetic field
+    b_theta: ndarray of floats
+        azimuhtal magnetic field
+    p_prime : ndarry of floats
+        derivative of pressure
+    Returns
+    -------
+    beta : ndarray of floats
+        beta of quadratic equation for the exponents of the Forbenius solutions
     """
     return 2.*b_theta**2/(b_theta + b_z)**2 * p_prime
 
@@ -38,6 +69,20 @@ def beta_func(b_z, b_theta, p_prime, **kwargs):
 def nu_1_2(alpha, beta, **kwargs):
     r"""
     Return exponents of Frobenius solution.
+
+    Paramters
+    ---------
+    alpha : ndarray of floats
+        alpha of quadratic equation for the exponents of the Frobenius
+        solutions
+    beta : ndarray of floats
+        beta of quadratic equation for the exponents of the Forbenius solutions
+    Returns
+    -------
+    nu_1 : ndarray of floats
+         exponent 1 of Frobenius solution
+    nu_2 : ndarray of floats
+         exponent 1 of Frobenius solution
     """
     nu_1 = 0.5 + np.sqrt(0.25 + beta/alpha)
     nu_2 = 0.5 - np.sqrt(0.25 + beta/alpha)
@@ -46,6 +91,26 @@ def nu_1_2(alpha, beta, **kwargs):
 
 def sings_alpha_beta(r, b_z_spl, b_theta_spl, p_prime_spl):
     r"""
+    Returns alpha and beta at the singularties.
+
+    Parameters
+    ----------
+    r : ndarray of floats
+        positions of singularities
+    b_z_spl : scipy spline object
+        axial magnetic field
+    b_theta_spl : scipy spline object
+        azimuthal magnetic field
+    p_prime_spl : scipy spline object
+        derivative of pressure
+
+    Returns
+    -------
+    alpha : ndarry of floats
+        alpha of quadratic equation for the exponents of the Frobenius
+        solutions
+    beta : ndarray of floats
+        beta of quadratic equation for the exponents of the Forbenius solutions
     """
     b_z = b_z_spl(r)
     b_z_prime = b_z_spl.derivative()(r)
@@ -61,17 +126,68 @@ def sings_alpha_beta(r, b_z_spl, b_theta_spl, p_prime_spl):
 
 def sings_suydam_stable(r, b_z_spl, b_theta_spl, p_prime_spl):
     r"""
+    Returns bool array. True for singularities that are Suydam stable and False
+    for unstable singularities.
+
+    Parameters
+    ----------
+    r : ndarray of floats
+        singularity positions
+    b_z_spl : scipy spline object
+        axial magnetic field
+    b_theta_spl : scipy spline object
+        azimuthal magnetic field
+    p_prime_spl :scipy spline object
+        derivative of pressure
+
+    Returns
+    -------
+    stable_mask : ndarray of bool
+        bool array of Suydam stable singularities can be used as a mask for
+        indexing.
     """
     alpha, beta = sings_alpha_beta(r, b_z_spl, b_theta_spl, p_prime_spl)
     return suydam_stable(alpha, beta)
 
 
-def sing_small_solution(r_sing, offset, b_z_spl, b_theta_spl, p_prime_spl):
+def sing_small_solution(r_sing, offset, b_z_spl, b_theta_spl, p_prime_spl,
+                        q_spl, f_func):
     r"""
+    Returns small solution of Frobenius expansion near singularity in y form of
+    ODE set.
+
+    Parameters
+    ----------
+    r_sing : ndarray of floats
+        position of singularities
+    offset : float
+        offset from singularity for integration
+    b_z_spl : scipy spline object
+        axial magnetic field
+    b_theta_spl : scipy spline object
+        azimuthal magnetic field
+    p_prime_spl : scipy spline object
+        derivative of pressure
+    q_spl : scipy spline object
+        safety factor
+    f_func : function
+        function
+
+    Returns
+    -------
+    small_sol : ndarry of floats (2)
+        small solution inf y form of ODE equation set.
+
+    References
+    ----------
+    Newcomb (1960) Hydromagnetic Stability of a Diffuse Linear Pinch
     """
     alpha, beta = sings_alpha_beta(r_sing, b_z_spl, b_theta_spl, p_prime_spl)
     nu_1, nu_2 = nu_1_2(alpha, beta)
-    return small_solution(r_sing + offset, r_sing, nu_1, nu_2)
+    f_params = {}
+    f = f_func(**f_params)
+    small_sol = small_solution(r_sing + offset, r_sing, nu_1, nu_2)
+    small_sol[1] = small_sol
 
 
 def suydam_stable(alpha, beta):
@@ -80,20 +196,20 @@ def suydam_stable(alpha, beta):
 
     Parameters
     ----------
-    alpha : ndarray
-        radial test points
-
+    alpha : ndarray of floats
+        alpha of quadratic equation for the exponents of the Frobenius
+        solutions
     beta : scipy spline
-        axial magnetic field
+        beta of quadratic equation for the exponents of the Forbenius solutions
 
     Returns
     -------
-    suydam : ndarray
-
+    suydam : ndarray of bool
+        suydam stable
 
     Notes
     -----
-    Returned expression can be checked for elements <=0.
+    This ithe criterion for oscillatory solutions near singularity.
 
     Reference
     ---------
@@ -106,10 +222,30 @@ def suydam_stable(alpha, beta):
 
 def small_solution(r, r_sing, nu_1, nu_2):
     r"""
-    Returns xi and xi_der of the small solution close to a singularity.
+    Returns xi and f*xi_der of the small solution close to a singularity.
+
+    Parameters
+    ----------
+    r : float
+        position at which small solution is desired
+    r_sing : float
+        position of singularity
+    nu_1 : float
+        exponent 1 of Frobenius solution
+    nu_2 : float
+        exponent 2 of Frobenius solution
+
+    Returns
+    -------
+    small_sol : ndarry of floats (2)
+        small solution near singularity
+
+    Reference
+    ---------
+    Newcomb (1960) Hydromagnetic Stability of a Diffuse Linear Pinch (p. 243)
     """
     x = np.abs(r - r_sing)
     if nu_1 > nu_2:
-        return (x**-nu_2, -nu_2*x**(-nu_2 - 1.))
+        return np.array([x**-nu_2, -nu_2*x**(-nu_2 - 1.)])
     else:
-        return (x**-nu_1, -nu_1*x**(-nu_1 - 1.))
+        return np.array([x**-nu_1, -nu_1*x**(-nu_1 - 1.)])
