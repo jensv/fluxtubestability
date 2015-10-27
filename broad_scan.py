e4e58f8020e0949d6d3692dc84bb013b42b67d4d
Jens von der Linden <jensvonderlinden@gmail.com>
2015-10-27 09:39:00 -0700

    Script for starting a broad scan.
    The broad scan results will be stored in a new SQL database.

--- broad_scan.py
+++ broad_scan.py
@@ -12,16 +12,23 @@ import skin_core_scanner_simple as scss
 
 lambda_bar_space = [0.01, 6., 75]
 k_bar_space = [0.01, 3., 75]
-for skin_width in np.logspace(0.001, 0.9, 25):
-    for transition_width in np.logspace(0.001, (1. - skin_width)/2., 25):
+for skin_width in np.logspace(np.log10(0.001), np.log10(0.9), 5):
+    for transition_width in np.logspace(np.log10(0.001),
+                                        np.log10((1. - skin_width)/2.), 5):
         core_radius = 1. - 2.*transition_width - skin_width
-        for epsilon in np.logspace(0.01, 1., 25):
-            for points_skin in np.linspace(20, 200, 5):
-                for points_transition in np.linspace(50, 500, 5):
-                    scss.scan_lambda_k_space(lambda_bar_space, k_bar_space,
-                                             skin_width=skin_width,
-                                             transition_width=transition_width,
-                                             core_radius=core_radius,
-                                             epsilon=epsilon,
-                                             points_skin=points_skin,
-                                             points_transition=points_transition)
+        for epsilon in np.logspace(np.log10(0.01), np.log10(1.), 5):
+
+            assert skin_width > 0, 'Negative skin_width'
+            assert transition_width > 0, 'Negative transition_width'
+            assert core_radius > 0, 'Negative core_radius'
+            assert 1.1 >= epsilon >= 0,  'epsilon outside of [0,1] interval'
+            assert (0.9 < skin_width + 2.*transition_width + core_radius <
+                    1.1), 'total pinch radius not normalized to 1'
+
+#            for points_skin in np.linspace(20, 200, 5):
+#                for points_transition in np.linspace(50, 500, 5):
+            scss.scan_lambda_k_space(lambda_bar_space, k_bar_space,
+                                     skin_width_norm=skin_width,
+                                     transition_width_norm=transition_width,
+                                     core_radius_norm=core_radius,
+                                     epsilon=epsilon)
