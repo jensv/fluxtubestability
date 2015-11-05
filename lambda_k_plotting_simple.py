6c50046b0986ba7f6e3056a934c019edebb4ddcf
Jens von der Linden <jensvonderlinden@gmail.com>
2015-11-04 16:24:42 -0800

    Remove analytic conditions function, refactor anaytic comparison into independent function.
    The conditions function has moved to the new analytic_condition module.

--- lambda_k_plotting_simple.py
+++ lambda_k_plotting_simple.py
@@ -14,6 +14,7 @@ from future.builtins import (ascii, bytes, chr, dict, filter, hex, input,
 
 import numpy as np
 from scipy.special import kv, kvp
+import analytic_condition as ac
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import SymLogNorm, BoundaryNorm
@@ -23,20 +24,11 @@ sns.set_style('white')
 sns.set_context('poster')
 
 
-def conditions(k_bar, lambda_bar, epsilon, m, delta):
-    term1 = (2*k_bar - m*epsilon*lambda_bar)*((delta + 1)*2*k_bar -
-                                              (delta - 1)*m*epsilon*
-                                              lambda_bar)/(k_bar**2 + m**2)
-    term2 = (epsilon**2 - 1) * lambda_bar**2
-    term3 = (m*lambda_bar - 2* k_bar)**2/k_bar * (kv(m, np.abs(k_bar)) /
-                                                  kvp(m, np.abs(k_bar)))
-    return term1 + term2 - term3
-
-
 def plot_lambda_k_space_dw(filename, epsilon, name, mode_to_plot='m_neg_1',
                            show_points=False, lim=None, levels=None, log=False,
                            linthresh=1E-7, bounds=(1.5, 3.0), floor_norm=False,
-                           analytic_compare=False, label_pos=((0.5,0.4),(2.1,0.4),(2.8, 0.2))):
+                           analytic_compare=False,
+                           label_pos=((0.5, 0.4), (2.1, 0.4), (2.8, 0.2))):
     r"""
     Plot the delta_w of external instabilities in the lambda-k space.
     """
@@ -108,29 +100,8 @@ def plot_lambda_k_space_dw(filename, epsilon, name, mode_to_plot='m_neg_1',
     lambda_bar_mesh, k_bar_mesh = np.meshgrid(lambda_bar, k_bar)
 
     if analytic_compare:
-        line_labels = FixedFormatter(['-1', '0', '1'])
-        if mode_to_plot == 'm_neg_1':
-            m = 1
-            color = 'red'
-        if mode_to_plot == 'm_0':
-            m = 0
-            color = 'red'
-
-        stability_kink_d_neg_1 = conditions(k_bar_mesh, lambda_bar_mesh, epsilon, m, -1.)
-        stability_kink_d_0 = conditions(k_bar_mesh, lambda_bar_mesh, epsilon, m, 0.)
-        stability_kink_d_1 = conditions(k_bar_mesh, lambda_bar_mesh, epsilon, m, 1)
-
-        stability_kink = stability_kink_d_neg_1 < 0
-        stability_kink = stability_kink.astype(float)
-        stability_kink[stability_kink_d_neg_1 >= 0] = -1.5
-        stability_kink[stability_kink_d_neg_1 < 0] = -0.5
-        stability_kink[stability_kink_d_0 < 0] = 0.5
-        stability_kink[stability_kink_d_1 < 0] = 1.5
-
-        cs = plt.contour(lambda_bar_mesh, k_bar_mesh, stability_kink,
-                         levels=[-1, 0, 1], colors=color, linewidths=5, linestyles = 'dotted')
-
-        plt.clabel(cs, fmt={-1: r'$\delta = -1$', 0: r'$\delta = 0$', 1: r'$\delta = 1$'}, fontsize=40, manual=label_pos)
+        analytic_comparison(mode_to_plot, k_bar_mesh, lambda_bar_mesh, epsilon,
+                            label_pos)
 
     if show_points:
         plt.scatter(lambda_a_mesh, k_a_mesh, marker='o', c='b', s=5)
@@ -146,3 +117,95 @@ def plot_lambda_k_space_dw(filename, epsilon, name, mode_to_plot='m_neg_1',
     plt.tight_layout()
     plt.savefig('../../output/plots/' + name + '.png')
     plt.show()
+
+
+def analytic_comparison(mode_to_plot, k_bar_mesh, lambda_bar_mesh, epsilon,
+                        label_pos):
+    r"""
+    Add red lines indicating stability boundaries from analytical model.
+    """
+    line_labels = FixedFormatter(['-1', '0', '1'])
+    if mode_to_plot == 'm_neg_1':
+        m = 1
+        color = 'red'
+    if mode_to_plot == 'm_0':
+        m = 0
+        color = 'red'
+
+    stability_kink_d_neg_1 = ac.conditions(k_bar_mesh, lambda_bar_mesh,
+                                           epsilon, m, -1.)
+    stability_kink_d_0 = ac.conditions(k_bar_mesh, lambda_bar_mesh,
+                                       epsilon, m, 0.)
+    stability_kink_d_1 = ac.conditions(k_bar_mesh, lambda_bar_mesh,
+                                       epsilon, m, 1)
+
+    stability_kink = stability_kink_d_neg_1 < 0
+    stability_kink = stability_kink.astype(float)
+    stability_kink[stability_kink_d_neg_1 >= 0] = -1.5
+    stability_kink[stability_kink_d_neg_1 < 0] = -0.5
+    stability_kink[stability_kink_d_0 < 0] = 0.5
+    stability_kink[stability_kink_d_1 < 0] = 1.5
+
+    cs = plt.contour(lambda_bar_mesh, k_bar_mesh, stability_kink,
+                     levels=[-1, 0, 1], colors=color, linewidths=5,
+                     linestyles='dotted')
+
+    plt.clabel(cs, fmt={-1: r'$\delta = -1$', 0: r'$\delta = 0$',
+                        1: r'$\delta = 1$'}, fontsize=40, manual=label_pos)
+    return cs
+
+
+def plot_lambda_k_space_delta(filename, mode_to_plot,
+                              clip=False, delta_min=-1.5,
+                              delta_max=1., levels=None):
+    r"""
+    Plot values of delta in lambda k space.
+    """
+    data_meshes = np.load(filename)
+    lambda_mesh = data_meshes['lambda_a_mesh']
+    k_mesh = data_meshes['k_a_mesh']
+    delta_mesh = data_meshes['delta_m_0']
+
+    if mode_to_plot == 0:
+        color = 'green'
+        delta_mesh = data_meshes['delta_m_0']
+    else:
+        #color = sns.xkcd_rgb["dandelion"]
+        color = 'green'
+        delta_mesh = data_meshes['delta_m_neg_1']
+
+    if clip:
+        delta_mesh = np.clip(delta_mesh, delta_min, delta_max)
+
+    colors = sns.light_palette(color, n_colors=6, reverse=True,
+                               as_cmap=True)
+
+    if levels:
+        plt.contourf(lambda_mesh, k_mesh, delta_mesh, cmap=colors,
+                     levels=levels)
+    else:
+        plt.contourf(lambda_mesh, k_mesh, delta_mesh, cmap=colors)
+
+    cbar = plt.colorbar(label=r'$\delta$')
+    cbar.set_label(label=r'$\delta$', size=45, rotation=0, labelpad=30)
+
+    if levels:
+        contourlines = plt.contour(lambda_mesh, k_mesh, delta_mesh,
+                                   colors='grey', levels=levels)
+    else:
+        contourlines = plt.contour(lambda_mesh, k_mesh, delta_mesh,
+                                   colors='grey')
+
+    cbar.add_lines(contourlines)
+
+    plt.ylim(0.01, 1.5)
+    plt.xlim(0.01, 3.0)
+    axes = plt.gca()
+    axes.set_axis_bgcolor(sns.xkcd_rgb['grey'])
+    plt.setp(axes.get_xticklabels(), fontsize=40)
+    plt.setp(axes.get_yticklabels(), fontsize=40)
+    plt.ylabel(r'$\bar{k}$', fontsize=45, rotation='horizontal', labelpad=30)
+    plt.xlabel(r'$\bar{\lambda}$', fontsize=45)
+    cbar.ax.tick_params(labelsize=40)
+    sns.despine(ax=axes)
+    plt.tight_layout()
