d474b4fcf988e138fb6419c7e575348d800576ac
Jens von der Linden <jensvonderlinden@gmail.com>
2016-12-22 12:22:54 -0800

    fix skin_core_scanner commands.

--- recreate_paper_data.sh
+++ recreate_paper_data.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 python init_database.py
-python --epsilon 0.7 --suppress_output 
-python --epsilon 0.5 --suppress_output
-python --epsilon 0.1 --suppress_output
+python skin_core_scanner.py --epsilon 0.7 --suppress_output 
+python skin_core_scanner.py --epsilon 0.5 --suppress_output
+python skin_core_scanner.py --epsilon 0.1 --suppress_output
